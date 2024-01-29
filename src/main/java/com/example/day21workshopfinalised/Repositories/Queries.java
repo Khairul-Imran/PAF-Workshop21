9f7ff2b2882b883d15ffe129e3674edf02088ea5
Khairul <Khairulimran6810@gmail.com>
2024-01-29 22:16:00 +0800

    Made a small change in the Queries.java file

--- src/main/java/com/example/day21workshopfinalised/Repositories/Queries.java
+++ src/main/java/com/example/day21workshopfinalised/Repositories/Queries.java
@@ -3,7 +3,7 @@ package com.example.day21workshopfinalised.Repositories;
 public class Queries {
     
     public static final String SQL_GET_ALL_CUSTOMERS = """
-            select * from customers limit ? offset ?;
+            select * from customers limit ? offset ?
             """;
 
     public static final String SQL_GET_CUSTOMER_BY_ID = """
